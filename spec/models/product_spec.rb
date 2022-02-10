6d854c7ef2541087ca6b022f3f7a6d4be5025bdf
Leshan Patterson <leshan0627@gmail.com>
2022-02-10 01:45:34 +0000

    Model spec: User

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,32 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  describe 'validations' do
-    it "it saves a product with all four fields successfully" do 
-      @category = Category.create(:name => "Shoe")
-      @product = Product.create({name: "Pink Shoe", price: 20, quantity: 2, category: @category})
-      expect(Product.count).to equal(1)
+  let(:category) {Category.new(:name => 'DC')}
+  subject {described_class.new(:name => 'Batman', :price => 300, :quantity => 30, :category => category)}  
+  
+  describe 'Validations' do
+  
+    
+    it 'saves successfully with all four fields set and belong to a category' do
+      subject.valid?
+      expect(subject.errors).to be_empty
+      assc = described_class.reflect_on_association(:category)
+      expect(assc.macro).to eq :belongs_to
     end
 
-    it "Does not save with incorrect name fields" do   
-      @category = Category.create(:name => "Shoe")
-      @product = Product.create({price: 20, quantity: 2, category: @category})
-      expect(Product.count).to equal(0)
+    it 'fails to save when name is not set' do
+      subject.name = nil
+      subject.valid?
+      expect(subject.errors).not_to be_empty
     end
-    it "Does not save with incorrect price fields" do   
-      @category = Category.create(:name => "Shoe")
-      @product = Product.create({name: "pink show", quantity: 2, category: @category})
-      expect(Product.count).to equal(0)
+    
+    it 'fails to save when price is not set' do
+      subject.price_cents = nil
+      subject.valid?
+      expect(subject.errors).not_to be_empty
     end
-    it "Does not save with incorrect quantity fields" do   
-      @category = Category.create(:name => "Shoe")
-      @product = Product.create({name: "pink show", price: 20, category: @category})
-      expect(Product.count).to equal(0)
+
+    it 'fails to save when quantity is not set' do
+      subject.quantity = nil
+      subject.valid?
+      expect(subject.errors).not_to be_empty
     end
-    it "Does not save with incorrect category fields" do   
-      @category = Category.create(:name => "Shoe")
-      @product = Product.create({name: "pink show", price: 20, quantity: 2})
-      expect(Product.count).to equal(0)
+
+    it 'fails to save when category is not set' do
+      subject.category = nil
+      subject.valid?
+      expect(subject.errors).not_to be_empty
     end
-  end
+
+
+  end  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,89 +2,147 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  subject {described_class.new(:first_name => 'Jon', :last_name => 'Jones', :email => 'bones@gamil.com', :password => "888", :password_confirmation => "888")}
+  describe 'Validations' do
+    it "is valid" do
+      user = User.new(
+        first_name: 'first_name',
+        last_name: 'last_name',
+        email: 'test2@test.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      expect(user).to be_valid
+    end
 
-  describe 'Validations do' do
+    it "email is missing" do
+      user = User.new(email: nil)
+      expect(user).to be_invalid
+      expect(user.errors[:email]).to include("can't be blank")
+  
+      user.email = 'test@test.com' # valid state
+      user.valid?
+      expect(user.errors[:email]).not_to include("can't be blank")
+    end
+
+    it "first name is missing" do
+      user = User.new(first_name: nil)
+      expect(user).to be_invalid
+      expect(user.errors[:first_name]).to include("can't be blank")
+  
+      user.first_name = 'first_name' # valid state
+      user.valid? 
+      expect(user.errors[:first_name]).not_to include("can't be blank")
+    end
+
+    it "last name is missing" do
+      user = User.new(last_name: nil)
+      expect(user).to be_invalid
+      expect(user.errors[:last_name]).to include("can't be blank")
+  
+      user.last_name = 'last_name' # valid state
+      user.valid? 
+      expect(user.errors[:last_name]).not_to include("can't be blank")
+    end
+
+    it "password don't match" do
+      user = User.new(
+        first_name: 'first_name',
+        last_name: 'last_name',
+        email: 'test2@test.com',
+        password: 'password',
+        password_confirmation: 'pass'
+      )
+      user.valid?
+      expect(user.errors[:password_confirmation]).to be_present
+    end
+
+    it 'email must be unique' do
+      user = User.new
+      user.first_name = 'first_name'
+      user.last_name = 'last_name'
+      user.email = 'test@test.com'
+      user.password = 'password'
+      user.password_confirmation = 'password'
+
+      user.save
     
-    it 'saves successfully when all four feilds are set' do
-      subject.valid?
-      expect(subject.errors).to be_empty
-    end
-
-    it 'fails to save when email is not set' do
-      subject.email = nil
-      subject.valid?
-      expect(subject.errors).not_to be_empty
-    end
-
-    it 'fails to save when first name is not set' do
-      subject.first_name = nil
-      subject.valid?
-      expect(subject.errors).not_to be_empty
-    end
-
-    it 'fails to save when last name is not set' do
-      subject.last_name = nil
-      subject.valid?
-      expect(subject.errors).not_to be_empty
-    end
-
-    it 'fails to save when password is not set' do
-      subject.password = nil
-      subject.valid?
-      expect(subject.errors).not_to be_empty
-    end
-
-    it 'fails to save when password_confirmation is not set' do
-      subject.password_confirmation = nil
-      subject.valid?
-      expect(subject.errors).not_to be_empty
-    end
-
-    it 'fails to save when password and password_confirmation does not match' do
-      subject.password_confirmation = '456'
-      subject.valid?
-      expect(subject.errors).not_to be_empty
-    end
+      u = User.new
+      u.first_name = 'first_name'
+      u.last_name = 'last_name'
+      u.email = 'test@test.com'
+      u.password = 'password'
+      u.password_confirmation = 'password'
+      u.save
     
-    it 'fails to save when email is not unique (not case sensitive)' do
-      User.create(:first_name => 'Jon', :last_name => 'Jones', :email => 'bones@gamil.com', :password => "888", :password_confirmation => "888", :password_digest => "123")
-      subject.valid?
-      expect(subject.errors).not_to be_empty
+      expect(u.errors[:email].first).to eq('has already been taken')
     end
 
-    it 'fails to save when password and password_confirmation is less than length 3' do
-      subject.password = '12'
-      subject.password_confirmation = '12'
-      subject.valid?
-      expect(subject.errors).not_to be_empty
+    it 'password length less than 2 characters is invalid' do
+      user = User.new
+      user.first_name = 'first_name'
+      user.last_name = 'last_name'
+      user.email = 'test@test.com'
+      user.password = '12'
+      user.password_confirmation = '12'
+      expect(user).to be_invalid
     end
 
     describe '.authenticate_with_credentials' do
-      it 'returns user if succesfully authenticated' do
-        subject.save
-        user = User.authenticate_with_credentials('bones@gamil.com', '888')
-        expect(subject).to be == user
+      it 'should pass with valid credentials' do
+        user = User.new(
+          first_name: 'first_name',
+          last_name: 'last_name',
+          email: 'test@test.com',
+          password: 'password',
+          password_confirmation: 'password'
+        )
+        user.save
+  
+        user = User.authenticate_with_credentials('test@test.com', 'password')
+        expect(user).not_to be(nil)
       end
-
-      it 'returns nil if not successfully authenticated' do
-        subject.save
-        user = User.authenticate_with_credentials('bones@gamil.com', 'ggg')
-        expect(user).to be == nil
+  
+      it 'should not pass with invalid credentials' do
+        user = User.new(
+          first_name: 'first_name',
+          last_name: 'last_name',
+          email: 'test@test.com',
+          password: 'password',
+          password_confirmation: 'password'
+        )
+        user.save
+  
+        user = User.authenticate_with_credentials('test@test.com', 'pass')
+        expect(user).to be(nil)
       end
-
-      it 'authenticates and users if user type white space before / after email' do
-        subject.save
-        user = User.authenticate_with_credentials(' bones@gamil.com ', '888')
-        expect(subject).to be == user
+  
+      it 'should pass even with spaces present in email' do
+        user = User.new(
+          first_name: 'first_name',
+          last_name: 'last_name',
+          email: 'test@test.com',
+          password: 'password',
+          password_confirmation: 'password'
+        )
+        user.save
+  
+        user = User.authenticate_with_credentials('  test@test.com  ', 'password')
+        expect(user).not_to be(nil)
       end
-
-      it 'authenticates and users if users type lower and upper case in email' do
-        subject.save
-        user = User.authenticate_with_credentials('boNes@gAmil.com', '888')
-        expect(subject).to be == user
+  
+      it 'should pass even with caps present in email' do
+        user = User.new(
+          first_name: 'first_name',
+          last_name: 'last_name',
+          email: 'test@test.com',
+          password: 'password',
+          password_confirmation: 'password'
+        )
+        user.save
+  
+        user = User.authenticate_with_credentials('tesT@tEst.com', 'password')
+        expect(user).not_to be(nil)
       end
     end
-    
-  end
+  end  
 end
